86605cb41e3619ba4e739862a307aab5d9602fa9
Susan L. P.  E <sulpengel@gmail.com>
2022-05-30 18:49:15 -0300

    if ifelse e else

--- adivinhacao.py
+++ adivinhacao.py
@@ -12,9 +12,16 @@ chutado = int(chute)
 
 # Necessário fazer a conversão pois  o input vem com string
 
-if ( numero_secreto == chutado ):
+acertou = numero_secreto == chutado
+maior = chutado > numero_secreto
+menor = chutado < numero_secreto
+
+if (acertou):
      print("Você acertou")
 else:
-    print("você errou")
+    if(maior):
+        print("você errou, o número secreto é mais baixo")
+    elif(menor):
+        print("você errou, o número secreto é mais alto")
 
 print("FIM DO JOGO")
