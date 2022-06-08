65ee5f7cb459ed349f17984aaccb9b2a47366963
Susan L. P.  E <sulpengel@gmail.com>
2022-06-08 15:39:37 -0300

    desenvolvimento jogo da forca, inclusão for

--- forca.py
+++ forca.py
@@ -4,5 +4,27 @@ def jogar():
     print("Bem vindo ao jogo da forca")
     print("*****************************")
 
+    palavra_secreta = "secreta"
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 1
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+                index = index + 1
+
+        print("Jogando...")
+
+        print("Fim do jogo")
+
+
 if (__name__ == "__main__"):
     jogar()
+
+    #função principal.
