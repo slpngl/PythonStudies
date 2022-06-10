ba95c3718b56e5f1c7b9ddf6914a7ada3cb44ab1
Susan L. P.  E <sulpengel@gmail.com>
2022-06-10 18:06:49 -0300

    listas

--- forca.py
+++ forca.py
@@ -5,23 +5,27 @@ def jogar():
     print("*****************************")
 
     palavra_secreta = "secreta"
+    letras_acertadas = ["_","_","_","_","_","_","_"]
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while (not acertou and not enforcou):
 
         chute = input("Qual letra? ")
         chute = chute.strip()
+        index = 0
 
-        index = 1
         for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-                index = index + 1
+              #  print("Encontrei a letra {} na posição {}".format(letra, index))
+              letras_acertadas[index] = letra
+            index = index + 1
 
-        print("Jogando...")
+        print(letras_acertadas)
 
-        print("Fim do jogo")
+    print("Fim do jogo")
 
 
 if (__name__ == "__main__"):
